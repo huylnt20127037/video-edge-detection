3957eef07c2b410814bdfea71ae3f8f207b4e1ad
huylnt <lnthuy20@clc.fitus.edu.vn>
2024-03-26 10:01:16 +0700

    Hiển thị từng khung của video lên canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { Flex } from '@chakra-ui/react'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 function App() {
-
+  const videoRef = useRef()
+  const canvasRef = useRef()
+  
   const buildVideoPlayer = () => {
-    return <video width='50%' controls>
+    return <video width='500' ref={videoRef} controls onTimeUpdate={() => canvasRef.current.getContext('2d').drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height) }>
     <source src='./sample.mp4' type='video/mp4' />
   </video>
   }
 
   const buildEdgeDetector =() => {
-    return <video width='50%' controls>
-    <source src='./sample.mp4' type='video/mp4' />
-  </video>
+    return <canvas id='canvas' ref={canvasRef} width='500' height='262' style={{'border': '1px solid black'}}></canvas>
   }
 
-  return <Flex justify='center' align='center' gap='36px' margin='10%'>
+  return <Flex justify='center' align='center' gap='108px' margin='10%'>
     {buildVideoPlayer()}
     {buildEdgeDetector()}
   </Flex> 
